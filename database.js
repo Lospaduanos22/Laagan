e7a83e2ef94fdfbd501b6671de5f75962a9bffc7
Jan Carlo Cabantoc <22102839@usc.edu.ph>
2024-11-13 13:38:45 +0800

    test

--- database.js
+++ database.js
@@ -1,1 +1,28 @@
-const express = require("express")
+// db.js
+const { MongoClient } = require('mongodb');
+
+// Load environment variables (assuming you are using dotenv for env variables)
+require('dotenv').config();
+
+const uri = process.env.MONGODB_URI || "mongodb+srv://laagantravel:<db_password>@lospaduanos.qpnae8g.mongodb.net/?retryWrites=true&w=majority&appName=Lospaduanos";
+
+async function connectToDatabase() {
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  try {
+    // Connect to the MongoDB cluster
+    await client.connect();
+    console.log("Connected to MongoDB!");
+
+    // Return the client to use it in other parts of your app
+    return client;
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    throw err;
+  }
+}
+
+module.exports = connectToDatabase;
